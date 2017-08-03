2b7572d151518230f893201ef506f3b997f1994f
Mallik Begur <mbegur@gmail.com>
2017-08-03 00:21:45 -0700

    Fix algorithims

---
+++ frontend/algorithms/bfs.js
@@ -0,0 +1,27 @@
+import Search from './search';
+import Queue  from '../queues/queue';
+
+class BFS extends Search {
+  initializeFrontier() {
+    this.frontier = new Queue();
+
+    this.processNeighbors(this.board.start);
+  }
+
+  processNeighbors(current) {
+    this.board.neighbors(current).forEach(
+      function(neighbor) {
+        if (!(neighbor in this.cameFrom)) {
+          const type = this.board.grid[neighbor].type;
+          if (type !== 'obstacle') {
+            this.frontier.enqueue(neighbor);
+            this.cameFrom[neighbor] = current;
+            this.board.grid[neighbor].setType('frontier');
+          }
+        }
+      }.bind(this)
+    );
+  }
+}
+
+export default BFS;
--- frontend/algorithms/dijkstra.js
+++ frontend/algorithms/dijkstra.js
@@ -1,7 +1,7 @@
 import Search from './search';
 import PriorityQueue  from '../queues/priority_queues';
 
-class AStar extends Search {
+class Dijkstra extends Search {
   initializeFrontier() {
     this.frontier = new PriorityQueue();
     this.processNeighbors(this.board.start);
@@ -17,21 +17,19 @@ class AStar extends Search {
     this.board.neighbors(current).forEach(
       function(neighbor) {
         const type = this.board.grid[neighbor].type;
-        const cost = type === 'obstacle' ? 99999 : 1;
+        const cost = type === 'obstacle' ? 100 : 1;
         const newCost = this.costSoFar[current] + cost;
 
         if (!(neighbor in this.costSoFar) ||
             newCost < this.costSoFar[neighbor]) {
-              const priority = newCost + this.euclidean(neighbor, this.board.goal);
-
-              this.frontier.put(neighbor, priority);
-              this.cameFrom[neighbor] = current;
-              this.costSoFar[neighbor] = newCost;
-              this.board.grid[neighbor].setType('frontier');
+            this.frontier.put(neighbor, newCost);
+            this.cameFrom[neighbor] = current;
+            this.costSoFar[neighbor] = newCost;
+            this.board.grid[neighbor].setType('frontier');
           }
         }.bind(this)
       );
   }
 }
 
-export default AStar;
+export default Dijkstra;
